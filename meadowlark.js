0233162da86f4a611dc3049f632a83d0959b9281
art <qvazimodo@mail.ru>
2022-07-12 22:47:34 +0300

    пляски с шаблонизатором express-handlebars, застрял на хелперах

--- meadowlark.js
+++ meadowlark.js
@@ -5,14 +5,24 @@ const handlers = require('./lib/handlers')
 const app = express()
 
 
-
+expressHandlebars.create({
+        helpers: {
+            section: function(name, options) {
+                if(!this._sections) this._sections = {}
+                this._sections[name] = options.fn(this)
+                return null
+            }
+        }
+})
 
 //Настройка механизма представления Handlebars
 app.engine('handlebars', expressHandlebars.engine(
     {
         layoutsDir: "views/layouts",
         defaultLayout: 'main'
-    }
+    },
+
+
 ))
 app.set('view engine', "handlebars")
 
@@ -23,12 +33,24 @@ app.use(express.static(__dirname + '/public'))
 
 app.get('/',handlers.home)
 app.get('/about',handlers.about)
+app.get('/jquery',handlers.jquery)
+
+app.get('/headers', (req,res) =>{
+console.log(res)
+    res.type('text/plan')
+
+    const headers = Object.entries(req.headers)
+        .map(([key,value]) => `${key}: ${value}`)
+    res.send(headers.join('\n'))
+})
+
 
 // Пользовательсткая страница 404
 app.use(handlers.notFound)
 
 // Пользовательская страница 500
 app.use(handlers.serverError)
+
 
 
 
